be192d2d36fe33e3e887f46a799cfbe3cace7262
Alexander Okenczyc <60657713+igicode2020@users.noreply.github.com>
2026-03-28 18:43:49 -0400

    Delete presageAPI directory

--- presageAPI/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PRESAGE_API_KEY = os.getenv("PRESAGE_API_KEY")
-HOST = os.getenv("HOST", "0.0.0.0")
-PORT = int(os.getenv("PORT", 5000))
-BASE_API_URL = "https://api.physiology.presagetech.com"
